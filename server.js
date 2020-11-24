9fb8accc5e6eca6bb2480f377a9f44344c423e46
Taki <laszlo@letmewebyou.com>
2020-11-24 08:21:12 +0100

    Removed mongo added axios

--- server.js
+++ server.js
@@ -1,43 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 require('dotenv').config();
 const { lookup } = require('geoip-lite');
+const axios = require('axios')
 
 const app = express();
 
 app.unsubscribe(bodyParser.urlencoded({extended:true}))
 app.unsubscribe(bodyParser.json())
 
-mongoose.connect('mongodb://'+ process.env.DATABASE +'/Subscribers', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}, function(error) {
-  if (error) console.log(error)
-  console.log("DB connected")
-})
 
-
-let Schema = mongoose.Schema
-let emailSchema = new Schema ({
-  email: String,
-  currDate: Date,
-  ip: String,
-})
-
-const Model = mongoose.model
-const Subscriber = Model ('Subscribers', emailSchema)
-
-app.get('/', (req,res) => {
+app.get('/bf', (req,res) => {
 let queryEmail = req.query.email
 const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
-let signup = new Subscriber ({
-  email: queryEmail,
-  currDate: new Date(),
-  ip: ip
-})
-signup.save().then(res.redirect('https://'+process.env.TARGET+'/?flag=blackfriday&email=' + queryEmail))
 
+axios.get(process.env.INTEGRO,{
+  params: {
+    email: queryEmail,
+    ip: ip,
+    currDate: new Date()
+  }
+}).then(function(res){console.log(res)}).catch(err => console.log(err)).then(res.redirect('https://'+process.env.TARGET+'/?flag=blackfriday&email=' + queryEmail))
 })
-
 app.listen(process.env.PORT || 3000)
+//Author: Taki
+
